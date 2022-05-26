ecc5484e7b8ceb52f8a60fc8f0a3aabcb696f5a3
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-05-26 14:01:24 +0200

    +

--- app.js
+++ app.js
@@ -24,12 +24,12 @@
 //     console.log(newWord);
 // }
 
-// for (let p = 1; p <= 10; p++) {
-//     console.log('gods Loop:', p);
-//     for (let q = 10; q >= 0; q -= 2) {
-//         console.log('  godess Loop', q);
-//     }
-// }
+for (let p = 1; p <= 10; p++) {
+    console.log('gods Loop:', p);
+    for (let q = 10; q >= 0; q -= 2) {
+        console.log('  godess Loop', q);
+    }
+}
 
 const sub = ['team', 'plays', 'diamond'];
 const subb = ['-player', ' basketball', '-castle'];
