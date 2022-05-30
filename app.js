df01f2471660540a2ff037510fa8929b1ef64852
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-05-30 08:44:50 +0200

    +

--- app.js
+++ app.js
@@ -11,3 +11,18 @@ for (let i = 0; i < sub.length; i++) {
     console.log(`${sub[i]}${subb[i]}`);
 }
 
+const x = 6;
+
+if (x === 5) {
+    console.log('monkey');
+} else if (x < 5) {
+    console.log('small monkey');
+} else {
+    console.log('lost creature');
+}
+
+const sumNum = (num1 = 1, num2 = 1) => {
+    console.log(num1 + num2);
+};
+
+sumNum(5.633564, 2333333335);
