5e39044d96fe426bf62704af6b7bcdc77bb2caa7
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-05-31 11:52:08 +0200

    Styles

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-for (let p = 1; p <= 10; p++) {
-    console.log('gods Loop:', p);
-    for (let q = 10; q >= 0; q -= 2) {
-        console.log('  godess Loop', q);
-    }
-}
+// for (let p = 1; p <= 10; p++) {
+//     console.log('gods Loop:', p);
+//     for (let q = 10; q >= 0; q -= 2) {
+//         console.log('  godess Loop', q);
+//     }
+// }
 
-const sub = ['team', 'plays', 'diamond'];
-const subb = ['-player', ' basketball', '-castle'];
-for (let i = 0; i < sub.length; i++) {
-    console.log(`${sub[i]}${subb[i]}`);
-}
+// const sub = ['team', 'plays', 'diamond'];
+// const subb = ['-player', ' basketball', '-castle'];
+// for (let i = 0; i < sub.length; i++) {
+//     console.log(`${sub[i]}${subb[i]}`);
+// }
 
-const x = 6;
+// const x = 6;
 
-if (x === 5) {
-    console.log('monkey');
-} else if (x < 5) {
-    console.log('small monkey');
-} else {
-    console.log('lost creature');
-}
+// if (x === 5) {
+//     console.log('monkey');
+// } else if (x < 5) {
+//     console.log('small monkey');
+// } else {
+//     console.log('lost creature');
+// }
 
-const sumNum = (num1 = 1, num2 = 1) => {
-    console.log(num1 + num2);
-};
+// const sumNum = (num1 = 1, num2 = 1) => {
+//     console.log(num1 + num2);
+// };
 
-sumNum(5.633564, 2333333335);
+// sumNum(5.633564, 2333333335);
 
-function Man(firstName, lastName, Ntnly) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.Ntnly = Ntnly;
-    this.Name = function () {
-        return `${this.firstName} ${this.Ntnly}`;
-    };
-}
+// function Man(firstName, lastName, Ntnly) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.Ntnly = Ntnly;
+//     this.Name = function () {
+//         return `${this.firstName} ${this.Ntnly}`;
+//     };
+// }
 
-const man1 = new Man('Charlie', 'bobo', 'kenyan');
+// const man1 = new Man('Charlie', 'bobo', 'kenyan');
 
-console.log(man1.Name());
+// console.log(man1.Name());
+
